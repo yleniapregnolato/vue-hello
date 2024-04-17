dd4d26e417a41cb868689edf4a2843732e28129a
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-17 13:08:03 +0200

    feat: bonus with image

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             greeting: "Hello world",
+            imgName: "https://i.etsystatic.com/7239025/r/il/b6c475/3373986585/il_1588xN.3373986585_jepv.jpg"
         }
     }
 }).mount('#app');
