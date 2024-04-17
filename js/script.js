520a782b9c3e927ea019486db5e23c808087158a
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-17 16:08:10 +0200

    feat: input with vue

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            greeting: "Hello world",
+            greeting: "",
             imgName: "https://i.etsystatic.com/7239025/r/il/b6c475/3373986585/il_1588xN.3373986585_jepv.jpg"
         }
     }
+    
 }).mount('#app');
