b100b524b4bd5c8e5100cbde8dd536ecc7f52e95
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-17 16:20:05 +0200

    add: button with color methods

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@ createApp({
     data() {
         return {
             greeting: "",
-            imgName: "https://i.etsystatic.com/7239025/r/il/b6c475/3373986585/il_1588xN.3373986585_jepv.jpg"
+            imgName: "https://i.etsystatic.com/7239025/r/il/b6c475/3373986585/il_1588xN.3373986585_jepv.jpg",
+            phrasecolor: "green"
+
+        }
+    },
+    methods: {
+        color: function () {
+            if (this.phrasecolor === "green") {
+                this.phrasecolor = "";
+            } else {
+                this.phrasecolor = "green";
+            } 
         }
     }
     
